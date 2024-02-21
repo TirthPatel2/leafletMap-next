7ba30b39a118090fdf67dd1980fc716a4535c4f8
= <la.tirthp@gmail.com>
2024-02-21 12:14:14 +0530

    updated some import modules

--- components/LeafMap.tsx
+++ components/LeafMap.tsx
@@ -2,8 +2,10 @@
 import React, { useEffect, useState } from 'react'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'
-import MarkerIcon from '@/../../node_modules/leaflet/dist/images/marker-icon.png';
-import MarkerShadow from '@/../../node_modules/leaflet/dist/images/marker-shadow.png';
+import MarkerIcon from 'leaflet/dist/images/marker-icon.png';
+import MarkerShadow from 'leaflet/dist/images/marker-shadow.png';
+// import MarkerIcon from '@/../../node_modules/leaflet/dist/images/marker-icon.png';
+// import MarkerShadow from '@/../../node_modules/leaflet/dist/images/marker-shadow.png';
 import L from 'leaflet';
 type PositionType = {
     lat: number,
@@ -22,20 +24,7 @@ const LeafMap = () => {
             .then(function (response) {
                 return response.json();
             })
-            .then(function (json) {
-                console.log(json);
-                console.log('Your country is ' + json['location']['country']['name']);
-            });
-
-        // const getUserIp = () => {
-        //     fetch("https://geolocation-db.com/json/41c35270-ca78-11ee-90e9-b72a16caa43d/").then(data => {
-        //         console.log(data);
-        //     }).catch(e => {
-        //         alert('user has blocked this');
-        //         console.log(e);
-        //     });
-        // }
-        // getUserIp();
+            .then((data) => setPosition({ lat: data.location.latitude, lng: data.location.longitude }));
     }, []);
     return (
         <>
@@ -64,8 +53,8 @@ const LeafMap = () => {
                                 shadowSize: [41, 41],
                             })
                         }
-                        position={{ lat: 21.18873, lng: 72.82927 }}
-                    // position={position}
+                        // position={{ lat: 21.18873, lng: 72.82927 }}
+                        position={position}
                     >
                         <Popup>
                             A pretty CSS3 popup. <br /> Easily customizable.
